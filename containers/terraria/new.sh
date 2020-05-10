0698339bd22a9564208aabddb64c638f65513a5c
oddamo <oamo@outlook.com>
2020-05-10 14:45:27 +0200

    BETA
    BETA

---
+++ containers/terraria/new.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+echo "Hva skal mappen den nye terraria verden bli laget som? Eksempel: ratpack-terraria-hardcore"
+echo "nb, kun små bokstaver"
+read -p 'navn:' world
+#
+sudo docker run -it \
+	-p 7788:7777 \
+	-v /home/docker/docker-volumes/${world}:/world \
+	--name="newworld" \
+	ryshe/terraria:latest
+
+
+
+
+
+
+
+
+
+
+
+
